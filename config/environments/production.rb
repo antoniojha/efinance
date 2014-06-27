2d6a5e15db36f714abd9fa166727b44d8931ff43
antoniojha <antoniojha@gmail.com>
2014-06-27 19:55:28 -0400

    prepare for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-WebFinance3::Aapplication.configure do
+WebFinance3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
