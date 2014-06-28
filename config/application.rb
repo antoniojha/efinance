367cfa5d94e260778d7230d0133bef3ec214525d
antoniojha <antoniojha@gmail.com>
2014-06-28 09:24:01 -0400

    prepare for Heroku

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module WebFinance3
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif) #line for asset pipeline compatibility
+    config.generators.test_framework :test_unit, fixture: true
   end
 end
