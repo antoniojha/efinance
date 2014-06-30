ab88f82cff2d2d03f2bfc2e6887ee38f2a94c3b6
antoniojha <antoniojha@gmail.com>
2014-06-29 20:29:11 -0400

    prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,41 +3,41 @@ WebFinance3::Application.routes.draw do
 
  resources :spendings
 
- # get "admin"=>'admin#index'
+ get "admin"=>'admin#index'
  controller :sessions do
     get 'login'=> :new
     post 'login'=> :create
     delete 'logout'=> :destroy
  end
   
- #get "overview"=>"profiles#new"
+ get "overview"=>"profiles#new"
  controller :profiles do
    get 'overview'=> :new
    post 'overview'=> :create
  end
   root 'sessions#new'
- # resources :search_spending
-  resources :users
+ resources :search_spending
+ resources :users
 
- # resources :finance_plans
+ resources :finance_plans
 
  resources :profiles
   
- # resources :finance_items
+ resources :finance_items
 
- # resources :temp_items
+ resources :temp_items
 
- # resources :goals
+ resources :goals
 
- # resources :recur_budgets
+ resources :recur_budgets
   
- # resources :budget_plans
- # resources :temp_budget_plans
- # resources :protection_plans
- # resources :emergency_plans
- # resources :college_plans
- # resources :retirement_plans
- # resources :investment_plans
- # resources :finance_goal
+ resources :budget_plans
+ resources :temp_budget_plans
+ resources :protection_plans
+ resources :emergency_plans
+ resources :college_plans
+ resources :retirement_plans
+ resources :investment_plans
+ resources :finance_goal
 
 end
